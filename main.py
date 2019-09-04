9a358bbfc3deb693551088656de6a20fac3157f8
amadeus <844082183@qq.com>
2019-09-04 16:15:56 +0800

    增加初始化方法
    todo
    修护harbor中的错误依赖

--- main.py
+++ main.py
@@ -2,9 +2,28 @@
 # -*- coding: utf-8 -*-
 # @Author  : Xie Chuyu
 # @Software: PyCharm
-from major import getIcon, getImages, tarThings, dockerThings, getAllCharts
+from chart import getIcon, getImages, tarThings, getAllCharts
+from harbor import dockerThings
+import os
+from config import config
+
+
+def init():
+    """
+    make sure the dir we need is exist
+
+    :return:
+    """
+    if not os.path.isdir('out'):
+        os.mkdir('out')
+    if not os.path.isdir(config['path']):
+        os.mkdir(config['path'])
+
+
+# todo add function to fix the wrong tag in harbor
 
 if __name__ == '__main__':
+    init()
     getAllCharts.get_all_tgz_url()
     getAllCharts.get_all_tgz()
     tarThings.find_and_un_tar()
